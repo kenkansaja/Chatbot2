4dee3ea67dc7f3da554194e0eb7bb214da926911
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2022-09-24 02:18:09 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
 import os 
 
 TOKEN = os.environ.get("TOKEN", "")
-OWNER = os.environ.get("OWNER", "kenkanasw")
-GROUP = os.environ.get("GROUP", "musikkugroup")
-CHANNEL = os.environ.get("CHANNEL", "musikkuchannel")
-BOT_NAME = os.environ.get("BOT_NAME", "Anonymous Chat Indonesia")
