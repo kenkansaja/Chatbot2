3d592252125f8fce42692cd970439f6e44fd48d6
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2022-09-24 10:44:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 import os 
 
 TOKEN = os.environ.get("TOKEN", "")
+OWNER = os.environ.get("OWNER", "kenkanasw")
+GROUP = os.environ.get("GROUP", "musikkugroup")
+CHANNEL = os.environ.get("CHANNEL", "musikkuchannel")
